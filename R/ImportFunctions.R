f1fc7931613e3b49b2178227ffd9c66e9cb10bb5
sccastro <cac200222@gmail.com>
2019-02-10 02:51:41 -0700

    Example Data added.

--- R/ImportFunctions.R
+++ R/ImportFunctions.R
@@ -1,9 +1,25 @@
 # Joel’s awesome function -------------------------------------------------
-#Use this function to read in a series of csv files and transform them into one data.frame
+#' Combine .csv Files
+#'
+#' Use this function to read in a series of .csv files and transform them into one data.frame. Make sure to save the output to a data.frame. See Examples for more.
+#'
+#' @param fpath The direcotry that contains all of the .csv files to be combined.
+#'
+#' @examples
+#'1+1
+# dat <- combineCSV(fpath = "data")
+#'
+#'@importFrom magrittr %>%
+#'@import dplyr
+#'@importFrom stringr str_split
+#'@importFrom stringr str_c
+#'@importFrom utils read.csv
+#'@export
 
-combineCSV <- function(fPath){ #see the r base cheatsheet for function syntax
+
+combineCSV <- function(fpath){ #see the r base cheatsheet for function syntax
   # List all files in the path
-  f <- list.files(path = fPath, recursive = TRUE) #see ?list.files
+  f <- list.files(path = fpath, recursive = TRUE) #see ?list.files
 
   #separate files list and place into tibble (This is all the names and subids in the titles of the files)
   f_split <- as_tibble(str_split(f, "_|/", simplify = TRUE)) %>% #see ?str_split from stringr package, separated by "_"
@@ -11,12 +27,15 @@ combineCSV <- function(fPath){ #see the r base cheatsheet for function syntax
     select(".id", everything()) #How does everything() work?
 
   #read in all files to list
-  fls <- lapply(str_c(fPath, f), read.csv, header = FALSE) #see ?str_c from stringr and ?lapply from the apply family
+  fls <- lapply(str_c(fpath, f), read.csv, header = FALSE) #see ?str_c from stringr and ?lapply from the apply family
 
   #convert list to data frame
-  d <- as.data.frame(rbindlist(fls, fill = TRUE, idcol = TRUE)) #see ?rbind_list from the data.table package
+  d <- as.data.frame(bind_rows(fls, fill = TRUE, idcol = TRUE)) #see ?rbind_list from the data.table package
 
   #join data with identifying information
   e <- f_split %>% #Join df of names and ids with df of data
     full_join(d, by = ".id") #See ?full_join
 }
+
+
+
---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' 13253 Detection Response Task Trials
+#'
+#' A dataset containing the 13253 trials of a detection response task
+#' with 20 participants.
+#'
+#' @format A data frame with 13253 rows and 8 variables:
+#' \describe{
+#'   \item{R}{Response 1 or 2 in choice stimulus tasks}
+#'   \item{condition}{Name and levels of experimental manipulation}
+#'   \item{response}{Either a "hit" or a "miss"}
+#'   \item{rt}{Response time in milliseconds}
+#'   \item{s1}{Stimulus one was presented}
+#'   \item{s2}{stimulus two was presented}
+#'   \item{subid}{subject identification number for twenty subjects}
+#'   \item{time}{Time in milliseconds}
+#'
+#'   ...
+#' }
+"drt.clean"
+
+
+
